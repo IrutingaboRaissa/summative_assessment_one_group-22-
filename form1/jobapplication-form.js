cde9915a6709d38b9cd5b5004e63a2190b67294f
phinah <phinah.mahoro10@gmail.com>
2024-06-19 10:46:05 +0200

    clear form on submit

--- form1/jobapplication-form.js
+++ form1/jobapplication-form.js
@@ -1,7 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
 const form = document.querySelector('form');
 const successMessage = document.querySelector('.success_text');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    // Clear the form fields
+        form.reset();
     successMessage.classList.add('show');
-    setTimeout(() => form.submit(), 20000);
-} );
+        // Optionally, hide the success message after a few seconds
+        setTimeout(function() {
+            successMessage.classList.remove('show');
+        }, 5000);
+    });
+});
